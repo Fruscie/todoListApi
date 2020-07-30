35de540abd94126a716d0f3e8bc097e6a18324f6
Fruscie <gilanggsb@gmail.com>
2020-07-30 22:24:37 +0700

    deploy

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     //ubah DB yang sudah di bold dengan host mongodb masing-masing
-    DB: process.env.HOST ? process.env.HOST : 'localhost',
+    DB: process.env.HOST ? process.env.HOST : 'localhostmongodb+srv://admin:inipassword@gg-6csk1.mongodb.net/learnit-react?retryWrites=true&w=majority',
     //learnit-react?retryWrites=true&w=majority',
-    APP_PORT: process.env.APP_PORT? process.env.APP_PORT:80,
+    APP_PORT: process.env.PORT? process.env.PORT:80,
 };
