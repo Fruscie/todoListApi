048fd2e873b676fdd9565d5ee402a03b09b54ab4
Fruscie <gilanggsb@gmail.com>
2020-07-30 22:33:11 +0700

    update

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     //ubah DB yang sudah di bold dengan host mongodb masing-masing
-    DB: process.env.HOST ? process.env.HOST : 'localhostmongodb+srv://admin:inipassword@gg-6csk1.mongodb.net/learnit-react?retryWrites=true&w=majority',
+    DB: process.env.HOST ? process.env.HOST : 'mongodb+srv://admin:inipassword@gg-6csk1.mongodb.net/learnit-react?retryWrites=true&w=majority',
     //learnit-react?retryWrites=true&w=majority',
     APP_PORT: process.env.PORT? process.env.PORT:80,
 };
