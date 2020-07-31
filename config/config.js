a156e812f468107eceb23568a4b336ddf718e729
Fruscie <gilanggsb@gmail.com>
2020-07-31 09:28:05 +0700

    ganti config

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    //ubah DB yang sudah di bold dengan host mongodb masing-masing
+    //ubah DB yang sudah di bold dengan host mongodb masing-masin
     DB: process.env.HOST ? process.env.HOST : 'localhost',
     //learnit-react?retryWrites=true&w=majority',
     APP_PORT: process.env.APP_PORT? process.env.APP_PORT:80,
